d17f838f0f7de2a0c415eab372f66ceee79ff493
shunsei-t <shunsei3t6@gmail.com>
2023-10-18 23:08:22 +0900

    add explain

--- merge_image.py
+++ merge_image.py
@@ -16,22 +16,29 @@ def merge_image(imgForeName, imgBackName, dx, dy, dc, dz, divSize):
     foreH, foreW = imgFore.shape[:2]
     backH, backW = imgBack.shape[:2]
 
-    # ((回転中心), 角度, 拡大率)
+    # 幾何変換行列の取得((回転中心), 角度, 拡大率)
     M = cv2.getRotationMatrix2D((foreH/2, foreW/2), dc, dz)
 
     M[0, -1] += int(dx - foreW/2)
     M[1, -1] += int(dy - foreH/2)
 
+    # アフィン変換（変換後、空白の領域は透過率0が入るはず）
     img_warped = cv2.warpAffine(imgFore, M, (backW, backH))
 
+    # 画像の重ね合わせ
+    # img_warpedの透過率（[:, :, 3]）の比率で、imgBackとimg_warpedの画素値を配分する。（）
+    # 例）imgBack[0, 0, 0] = imgBack[0, 0, 0]*(1-img_warped[0, 0, 3]/255) + img_warped[0, 0, 0]*(img_warped[1, 1, 3] / 255)
+    # 例）           31.76 =               20*(1-                100/255) +                   0*(100/255)
     imgBack[:, :, 0] = imgBack[:, :, 0] * (1 - img_warped[:, :, 3] / 255) + img_warped[:, :, 0] * (img_warped[:, :, 3] / 255)
     imgBack[:, :, 1] = imgBack[:, :, 1] * (1 - img_warped[:, :, 3] / 255) + img_warped[:, :, 1] * (img_warped[:, :, 3] / 255)
     imgBack[:, :, 2] = imgBack[:, :, 2] * (1 - img_warped[:, :, 3] / 255) + img_warped[:, :, 2] * (img_warped[:, :, 3] / 255)
 
+    # リサイズ
     imgResize = cv2.resize(imgBack, (int(backW/divSize), int(backH/divSize)))
 
     resizeH, resizeW = imgResize.shape[:2]
 
+    # 幾何変換後のimgForeに対する、Bouding boxの四つ角の位置
     rectCorners = [[int((dx - foreW*dz/2)/divSize), int((dy - foreH*dz/2)/divSize)],
                    [int((dx + foreW*dz/2)/divSize), int((dy - foreH*dz/2)/divSize)],
                    [int((dx + foreW*dz/2)/divSize), int((dy + foreH*dz/2)/divSize)],
